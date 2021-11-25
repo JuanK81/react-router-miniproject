c7b707fb5aa5cc45248e55dc90bab890340c4ecb
JuanK81 <jcasalsbadolato@gmail.com>
2021-11-25 17:03:09 +0100

    Persitent bug, asyc task on useEffect

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,14 +1,38 @@
-import QuoteList from '../components/quotes/QuoteList'
+import { useEffect } from 'react';
 
-const DUMMY_QUOTES = [
-    {id: 'q1', author:'Paquito', text:'Pablito clavó un clavito.'},
-    {id: 'q2', author:'Pepe', text:'Pablito no clavó un clavito, Fake News.'},
-    {id: 'q3', author:'Paquito', text:'Pepe, troll.'},
-]
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
+import QuoteList from '../components/quotes/QuoteList'
+import NoQuotesFound from '../components/quotes/NoQuotesFound'
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
 const AllQuotes = () => {
+    const { sendRequest, status, data: loadedQuotes, error} = useHttp(getAllQuotes, true);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    if (status === 'pending') {
+        return <div className='centered'>
+            <LoadingSpinner />
+        </div>
+    }
+
+    if (error) {
+        return(
+        <p className='centered focus'>{error}</p>)
+    }
+
+    if(status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+        return (
+            <NoQuotesFound />
+        )
+    }
+
     return (
-        <QuoteList quotes={DUMMY_QUOTES}/>
+        <QuoteList quotes={loadedQuotes}/>
     )
 };
 
