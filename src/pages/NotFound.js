1845e4efbbec605e10403a827ddcec2c3c98fd95
Juan <jcasalsbadolato@gmail.com>
2021-11-25 11:18:48 +0100

    Not foud handling

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+
+
+const NotFound = () => {
+    return (
+        <div className='centered'>
+            <h2>404</h2>
+            <p>Page Not Found.</p>
+        </div>
+    )
+};
+
+export default NotFound;
